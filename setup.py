867424794a384ebca3eb93bf2ff293fe6dddcef7
Yan <yanmarques@gmail.com>
2021-10-17 23:22:47 -0300

    fixed setup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup (
     name='pandas_pareto',
     packages=find_packages(),
-    install_requires=['pandas, numpy']
+    install_requires=['pandas', 'numpy']
 )
